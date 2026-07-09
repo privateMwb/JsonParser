ab5c3334d9823d5639afcb65dbaa5e8fa0d4c11d
Rain Lee <private.m.w.b.21@gmail.com>
2026-07-09 15:01:01 +0800

    refactor

--- tests/common/framework.h
+++ tests/common/framework.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <JsonPro/Json.h>
+#include <JsonPro/JsonObject.h>
+#include <JsonPro/Parser.h>
 
 #include "helper.h"
 #include "macros.h"
--- tests/common/macros.h
+++ tests/common/macros.h
@@ -63,7 +63,7 @@
         }                                    \
         CHK(threw);                         \
     } while (0)
-
+    
 // Registers this file's run_tests with the global registry so it runs
 // automatically at startup. The anonymous namespace gives `registrar`
 // internal linkage, preventing duplicate-symbol errors when this macro
